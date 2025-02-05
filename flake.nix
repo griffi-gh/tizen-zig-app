ed6da76f7c86106f1f2d292ea9c3a26998223769
griffi-gh <prasol258@gmail.com>
2025-02-05 15:48:23 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # zig = {
+    #   url = "github:mitchellh/zig-overlay";
+    #   inputs = {
+    #     nixpkgs.follows = "nixpkgs-stable";
+    #     flake-compat.follows = "";
+    #   };
+    # };
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          zig
+          zls
+        ];
+      };
+    });
+}
